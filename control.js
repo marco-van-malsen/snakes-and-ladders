552654b19c981ee33f8078bfd564782942869980
Marco van Malsen <37261543+marco-van-malsen@users.noreply.github.com>
2018-04-27 20:50:37 +0200

    Update comment

--- control.js
+++ control.js
@@ -1,5 +1,10 @@
 // each individual control
 class Control {
+  // (x,y) = upper left corner of control element
+  // (w,h) = width and height of control element
+  // (val) = value to show
+  // (fnc) = function to execute when clicked
+  // (active) = boolean to mark the control as the currently active one in the set
   constructor(x, y, w, h, val, fnc, active) {
     this.x = x;
     this.y = y;
@@ -43,8 +48,7 @@ class Control {
 // (max) = maximum value
 // (num) = number of choices
 // (fnc) = function to call when clicked
-// (type)= type of contole 'boolean' or 'value'
-function createControlSet(x, y, w, h, cur, min, max, num, fnc, type) {
+function createControlSet(x, y, w, h, cur, min, max, num, fnc) {
   // calculate interval
   let interval = (max - min) / (num - 1);
 
@@ -59,21 +63,22 @@ function createControlSet(x, y, w, h, cur, min, max, num, fnc, type) {
   for (let c = min; c <= max; c += interval) {
     // determine if current control-value matches the current value
     let active = false;
+    
+    // this is for booleans
     if (min === 0 && max === 1) {
-      // this is a boolean
       if ((c === 0 && cur === false) || (c === 1 && cur === true)) {
         active = true;
       }
 
-      // this is value/integer
+      // this is for values
     } else {
       if (c === cur) {
         active = true;
       }
     }
 
-    // create a new control
-    let control = new Control(myX, y, myW, h, myVal, fnc, active, type);
+    // create control element and add to array
+    let control = new Control(myX, y, myW, h, myVal, fnc, active);
     controls.push(control);
 
     // update for next control
@@ -90,6 +95,7 @@ function mousePressed() {
   }
 
   // check if the die was clicked
+  // only allowed when simulationMode is not active
   if (!simulationMode &&
     mouseX > die.x && mouseX < die.x + die.wh &&
     mouseY > die.y && mouseY < die.y + die.wh) {
@@ -97,7 +103,7 @@ function mousePressed() {
   }
 }
 
-// switch animation mode on or off
+// switch animation mode on/off
 function toggleAnimationMode() {
   // toggle animation mode
   animationMode = !animationMode;
@@ -119,12 +125,20 @@ function toggleGridSize(num) {
   if (num === cols) {
     return;
   }
+  
+  // update grid size
   cols = num;
   rows = cols;
+  
+  // set maximum possible snakes and ladder
   maxSnakes = cols * 0.5;
   maxLadders = maxSnakes;
+  
+  // set active number of snakes and ladders
   numSnakes = floor(cols / 3);
   numLadders = numSnakes;
+  
+  // start a new game
   initGame();
 }
 
